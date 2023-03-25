6d6e1c026274b04d69b85ce1a99415a48bd6cec9
kritigirnar <kritigirnar@gmail.com>
2023-03-25 16:50:50 +0400

    Create FluxException.java

---
+++ FluxException.java
@@ -0,0 +1,35 @@
+mport java.util.*;
+import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
+
+//import org.junit.jupiter.api.Test;
+
+public class Main {
+   public static void main(String[] args) {
+     // System.out.println("Hello, World!");
+      // @Test
+      //TestMono();
+      testFlux();
+}
+public static void TestMono()
+      {
+          Mono<?> monoObj= Mono.just("hello single object")
+          .then(Mono.error(new RuntimeException("exception thriwn from mono obj")))
+        .log();
+        monoObj.subscribe(System.out::println, a->
+        System.out.println(a.getMessage()));
+      
+ }
+ 
+ public static void testFlux()
+ {
+   Flux<String> fluxMultipleObject= 
+   Flux.just("kriti","mahi","mulu")
+   .concatWithValues("ammu")
+   .concatWith(Flux.error(new RuntimeException("fluxeceptyion")))
+   .log();
+   fluxMultipleObject.subscribe(System.out::println);
+ }
+ 
+ 
+}
